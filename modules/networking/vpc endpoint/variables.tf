7e43994c49cd36c79ca866650979e538da52cc76
theusc6 <117942576+theusc6@users.noreply.github.com>
2023-10-06 06:02:46 -0700

    Update variables.tf

--- modules/networking/vpc endpoint/variables.tf
+++ modules/networking/vpc endpoint/variables.tf
@@ -1,1 +1,28 @@
+variable "vpc_id" {
+  description = "The VPC ID where the endpoint will be created."
+  type        = string
+}
+
+variable "service_name" {
+  description = "The service name for the VPC endpoint."
+  type        = string
+}
+
+variable "endpoint_type" {
+  description = "The type of endpoint: Interface or Gateway."
+  default     = "Interface"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs to be associated with the endpoint."
+  type        = list(string)
+  default     = []
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the endpoint. Required for Interface type."
+  type        = list(string)
+  default     = []
+}
 
