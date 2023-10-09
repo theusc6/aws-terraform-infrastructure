6562fbba8263226e359d5b4dcff87dbe828043c9
theusc6 <117942576+theusc6@users.noreply.github.com>
2023-10-09 12:18:38 -0700

    Update backend.tf

--- global/backend.tf
+++ global/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-name"  # replace with your bucket name
     key            = "path/to/my/key"                  # the path within the bucket where the state will be stored
-    region         = "us-west-1"                       # the AWS region of the bucket
+    region         = "us-west-2"                       # the AWS region of the bucket
     dynamodb_table = "my-lock-table"                   # replace with your DynamoDB table name
     encrypt        = true
   }
