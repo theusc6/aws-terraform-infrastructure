10e07dc758f573ac5d8dd31597a73b41cfbf650a
theusc6 <uscteam6@gmail.com>
2023-10-09 15:46:52 -0700

    test commit

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,6 @@
 module "vpc_endpoint_s3" {
   source        = "path-to-your-module/vpc-endpoint-module"
-  vpc_id        = "vpc-12345678"
+  vpc_id        = "vpc-12345679"
   service_name  = "com.amazonaws.region.s3"
   endpoint_type = "Gateway"
 }
