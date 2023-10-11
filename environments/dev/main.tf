8ad8d70c1de8880c54e102fb80298938981b679a
theusc6 <117942576+theusc6@users.noreply.github.com>
2023-10-10 22:56:27 -0700

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,16 +1,1 @@
-resource "aws_s3_bucket" "test_deploy_bucket" {
-  bucket = "test-deploy-bucket"
-  acl    = "private"
 
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
