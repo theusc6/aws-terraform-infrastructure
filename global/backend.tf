6bb8940a073209593005b9d2715da385a45b74a8
theusc6 <117942576+theusc6@users.noreply.github.com>
2023-10-10 20:16:09 -0700

    Delete global/backend.tf

--- global/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-name"  # replace with your bucket name
-    region         = "us-west-2"                       # the AWS region of the bucket
-    dynamodb_table = "my-lock-table"                   # replace with your DynamoDB table name
-    encrypt        = true
-  }
-}
