e3c51cc981b1a97b4b9cfe6433dd7cc9c9dfa318
theusc6 <117942576+theusc6@users.noreply.github.com>
2023-10-23 12:00:26 -0700

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,7 @@
 resource "aws_account_alternate_contact" "security" {
   alternate_contact_type = "SECURITY"
 
-  name              = "Test Name"
+  name              = "Test Name1"
   title             = "Test Title2"
   phone_number      = "+1234567890"
   email_address     = "testemail@test.com"
