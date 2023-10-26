3d206633c060cf2d1875226ba6f90a11df8e7c15
theusc6 <117942576+theusc6@users.noreply.github.com>
2023-10-26 16:59:08 -0700

    Update providers.tf

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,4 +1,8 @@
-provider "aws" {
-  version = "~> 5.0"
-  region  = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
