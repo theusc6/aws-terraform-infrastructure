93f7dfa757e0c7aeb3cb9f44b834acd8e4e2a526
donf <117942576+theusc6@users.noreply.github.com>
2024-01-19 17:24:20 -0800

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,10 +1,60 @@
-module "vpc_endpoint_s3" {
-  source        = "github.com/theusc6/terraform-infrastructure//modules/networking/vpc-endpoint-module?ref=v0.1.0"
-  vpc_id        = "vpc-12345678"
-  service_name  = "com.amazonaws.region.s3"
-  endpoint_type = "Gateway"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "=3.30.0"
+    }
+  }
 }
 
-output "endpoint_id" {
-  value = module.vpc_endpoint_s3.vpc_endpoint_id
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_sqs_queue" "queue" {
+  name                      = "upload-queue"
+  delay_seconds             = 60
+  max_message_size          = 8192
+  message_retention_seconds = 172800
+  receive_wait_time_seconds = 15
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket  = "upload-bucket"
+  acl     = "private"
+  }
+
+resource "aws_s3_bucket_notification" "bucket_notif" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn   = aws_sqs_queue.queue.arn
+    events      = ["s3:ObjectCreated:*"]
+  }
+}
+
+data "aws_iam_policy_document" "iam_notif_policy_doc" {
+  statement {
+    id = 1
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.queue.arn]
+
+    condition {
+      test = "ArnEquals"
+      variable = "aws:SourceArn"
+      values = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "notif_policy" {
+  queue_url = aws_sqs_queue.queue.id
+  policy = data.aws_iam_policy_document.iam_notif_policy_doc.json
 }
